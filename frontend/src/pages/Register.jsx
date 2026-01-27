a398e9a2406262f79c7fe57d55f44e213ecaa20d
Naseer Pasha <n763122562@gmail.com>
2026-01-27 22:59:25 +0530

    --------------

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
         schoolName: '',
         directorName: '',
         email: '',
-        phone: '',
+        phone: '',   
         password: '',
         confirmPassword: ''
     });
